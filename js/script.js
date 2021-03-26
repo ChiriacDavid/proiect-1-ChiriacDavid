b7ad53b5f84c7a3edcffc130b3e3442ceea32b8f
ChiriacDavid <david.chiriac@student.tuiasi.ro>
2021-03-26 13:50:24 +0200

    task

--- js/script.js
+++ js/script.js
@@ -24,8 +24,56 @@ function showPosition(position) {
   "<br>Longitude: " + position.coords.longitude;
 }
 
+function browser(){
+  document.getElementById("brow").innerHTML =
+  "Name and version of the Browser: " + navigator.appVersion;
+}
+
+function os(){
+  var detectedOS = "Unknown OS";
+
+  if (navigator.appVersion.indexOf("Mac") != -1) 
+    detectedOS = "MacOS";
+
+  if (navigator.appVersion.indexOf("Win") != -1) 
+    detectedOS = "Windows";
+
+  if (navigator.appVersion.indexOf("Linux") != -1) 
+    detectedOS = "Linux";
+    
+  document.getElementById("OS").innerHTML=
+  "Operating system is: " + detectedOS;
+}
+
 function info(){
   refreshTime();
   url();
   getLocation();
+  browser();
+  os();
+}
+
+function extragere(){
+  var nr=0;
+  var numere = [];
+
+  let hexRef = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+
+  for(i=1;i<=8;i++){
+    numere[i] = hexRef[Math.floor(Math.random()*16)]+hexRef[Math.floor(Math.random()*16)];
+      for(j=0;j<i;j++){
+        while(numere[i]==numere[j]){
+          numere[i] = hexRef[Math.floor(Math.random()*16)]+hexRef[Math.floor(Math.random()*16)];
+        }
+      }
+  }
+  for(i=1;i<=8;i++){
+    for(j=1;j<=8;j++){
+      if(document.getElementById("formular").elements[i].value==numere[j]){
+        nr++;
+      }
+    }
+  }
+  alert("numerele extrase sunt: "+numere[1] +","+numere[2]+","+numere[3]+","
+      +numere[4]+","+numere[5]+","+numere[6]+","+numere[7]+","+numere[8])
 }
