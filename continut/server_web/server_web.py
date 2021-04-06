db6429ee1ab787285c5797a644445bf628d0f79e
ChiriacDavid <david.chiriac@student.tuiasi.ro>
2021-04-06 13:46:43 +0300

    Update server_web.py

--- continut/server_web/server_web.py
+++ continut/server_web/server_web.py
@@ -1,5 +1,5 @@
 import socket
-
+import os
 # creeaza un server socket
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # specifica ca serverul va rula pe portul 5678, accesibil de pe orice ip al serverului
@@ -30,6 +30,17 @@ while True:
     #DO interpretarea sirului de caractere `linieDeStart` pentru a extrage numele resursei cerute
     x = linieDeStart.split(" ")
     adresa=x[1]
+    adresa=adresa[1:len(adresa)]
     #DO trimiterea răspunsului HTTP
+    filename=os.path.join( os.getcwd(),'continut', adresa )
+    f=open(filename,'r')
+    clientsocket.sendall(str.encode("HTTP/1.0 200 OK\n",'ISO-8859-2'))
+    clientsocket.sendall(str.encode('Content-Type: text/html\n', 'ISO-8859-2'))
+    clientsocket.send(str.encode('\r\n'))
+    # send data per line
+    for l in f.readlines():
+        #print('Sent ', repr(l))
+        clientsocket.sendall(str.encode(""+l+"", 'ISO-8859-2'))
+        l = f.read(2048)
     clientsocket.close()
     print ('S-a terminat comunicarea cu clientul.')
