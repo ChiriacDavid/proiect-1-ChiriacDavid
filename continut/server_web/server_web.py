ef5d13952871021b65de4d2181f43dab8203124c
ChiriacDavid <david.chiriac@student.tuiasi.ro>
2021-04-06 14:04:05 +0300

    Update server_web.py

--- continut/server_web/server_web.py
+++ continut/server_web/server_web.py
@@ -32,15 +32,40 @@ while True:
     adresa=x[1]
     adresa=adresa[1:len(adresa)]
     #DO trimiterea răspunsului HTTP
-    filename=os.path.join( os.getcwd(),'continut', adresa )
-    f=open(filename,'r')
-    clientsocket.sendall(str.encode("HTTP/1.0 200 OK\n",'utf-8'))
-    clientsocket.sendall(str.encode('Content-Type: text/html\n', 'utf-8'))
-    clientsocket.send(str.encode('\r\n'))
+    filename=os.path.join( os.getcwd(),'continut', adresa)
+    #f=open(filename,'r')
+    #clientsocket.sendall(str.encode("HTTP/1.0 200 OK\n",'utf-8'))
+    #clientsocket.sendall(str.encode('Content-Type: text/html\n', 'utf-8'))
+    #clientsocket.send(str.encode('\r\n'))
     # send data per line
-    for l in f.readlines():
-        print('Sent ', repr(l))
-        clientsocket.sendall(str.encode(""+l+"", 'utf-8'))
-        l = f.read(1024)
+    #for l in f.readlines():
+    #    print('Sent ', repr(l))
+    #    clientsocket.sendall(str.encode(""+l+"", 'utf-8'))
+    #    l = f.read(1024)
+    
+    try:
+        file = open(filename,'rb') # open file , r => read , b => byte format
+        response = file.read()
+        file.close()
+
+        header = 'HTTP/1.1 200 OK\n'
+
+        if(filename.endswith(".jpg")):
+            mimetype = 'image/jpg'
+        elif(filename.endswith(".css")):
+            mimetype = 'text/css'
+        else:
+            mimetype = 'text/html'
+
+        header += 'Content-Type: '+str(mimetype)+'<strong>\n\n</strong>'
+
+    except Exception as e:
+        header = 'HTTP/1.1 404 Not Found\n\n'
+        response = '<html> <body> <center> <h3>Error 404: File not found</h3> <p>Python HTTP Server</p> </center></body></html>'.encode('utf-8')
+
+    final_response = header.encode('utf-8')
+    final_response += response
+    clientsocket.send(final_response)
+
     clientsocket.close()
     print ('S-a terminat comunicarea cu clientul.')
